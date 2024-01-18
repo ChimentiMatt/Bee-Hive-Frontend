900a0c9afb7f99dd24f41ff681c8828aa2b6cc33
ChimentiMatt <chimcode@gmail.com>
2024-01-18 15:26:47 -0800

    make it better

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-
 const express = require('express')
-const port = process.env.PORT || 8080
+const serveStatic = require('serve-static')
+const path = require('path')
+
+// initialise the express package
 const app = express()
 
-app.use(express.static(__dirname + "/dist"))
-app.get(/.*/, function(req, res) {
-    res.sendFile(__dirname + "/dist/index.html")
-})
-app.listen(port);
+// use the serve-static package to serve the bundled app files in the dist directory
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-console.log('Server started...')
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+/**
+ * heroku automatically assigns port so leave it to do it's
+ * work, don't set a port in the heroku dashboard. while the
+ * 5000 or whatever number you set will be for your local
+ * machine.
+ */
+const port = process.env.PORT || 5000
+app.listen(port)
+console.log(`app is listening on port: ${port}`)
