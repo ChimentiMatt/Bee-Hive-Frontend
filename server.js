7aec07bc0db8f520eb55c386c4fd70eec18112c3
ChimentiMatt <chimcode@gmail.com>
2024-01-18 15:42:06 -0800

    make it better

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 import { createRequire } from 'module';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const require = createRequire(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const express = require('express')
 const serveStatic = require('serve-static')
