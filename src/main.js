b784852c7ac8d35e20d57e398bbea7ab4d6ea21e
ChimentiMatt <chimcode@gmail.com>
2024-01-18 15:19:37 -0800

    make it better

--- src/main.js
+++ src/main.js
@@ -7,7 +7,8 @@ import axios from 'axios'
 
 import './assets/main.css'
 
-// axios.defaults.baseURL = import.meta.env.VITE_API_URL
+axios.defaults.baseURL = import.meta.env.VITE_API_URL
+
 
 const app = createApp(App)
 
